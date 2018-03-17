3b04d0ac8189a5515c2463ab6db3b5984979b66a
Horia Coman <horia141@gmail.com>
2018-03-17 13:49:50 +0200

    Update ENV->config structure

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,12 +1,8 @@
 import { getNamespace } from 'continuation-local-storage'
-import { config } from 'dotenv'
 
 import { Context, Env, parseContext, parseEnv, isOnServer } from '@truesparrow/common-js'
 import { getFromEnv } from '@truesparrow/common-server-js'
 import { Session } from '@truesparrow/identity-sdk-js'
-
-
-config();
 
 
 export const CLS_NAMESPACE_NAME: string = 'truesparrow.request';
